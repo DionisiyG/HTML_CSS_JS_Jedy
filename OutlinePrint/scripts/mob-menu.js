8ede9456463ec6a1b4a033b1ccbfc0804a874f20
di0nise4ka <byba.dolce@gmail.com>
2017-08-14 11:03:39 +0300

    Bug fixes

--- OutlinePrint/scripts/mob-menu.js
+++ OutlinePrint/scripts/mob-menu.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
-    $('.mob-menu').on('click', function() {
-        $('.mob-nav').slideToggle(400);
-    });
+    function detectBrowserSize() {
+        var myWidth = 0;
+        if (typeof (window.innerWidth) == 'number') {
+            //Non-IE
+            myWidth = window.innerWidth;
+        } else if (document.documentElement && document.documentElement.clientWidth) {
+            //IE 6+ in 'standards compliant mode'
+            myWidth = document.documentElement.clientWidth;
+        } else if (document.body && document.body.clientWidth) {
+            //IE 4 compatible
+            myWidth = document.body.clientWidth;
+        }
+        return myWidth;
+    }
+        if (detectBrowserSize() < 321 ){
+            $('.mob-menu').on('click', function() {
+                $('.mob-nav').slideToggle(400);
+            });
+         }
+         else {
+            $("#mob-wrapper").css("display", "none");
+        }
+
+
 });
--- OutlinePrint/scripts/nav-fixer.js
+++ OutlinePrint/scripts/nav-fixer.js
@@ -1,19 +1,6 @@
 
 $(document).ready(function(){
-    function detectBrowserSize() {
-        var myWidth = 0;
-        if (typeof (window.innerWidth) == 'number') {
-            //Non-IE
-            myWidth = window.innerWidth;
-        } else if (document.documentElement && document.documentElement.clientWidth) {
-            //IE 6+ in 'standards compliant mode'
-            myWidth = document.documentElement.clientWidth;
-        } else if (document.body && document.body.clientWidth) {
-            //IE 4 compatible
-            myWidth = document.body.clientWidth;
-        }
-        return myWidth;
-    }
+
     var $fixed = $("#nav-fixed");
     $(window).scroll(function(){
         if ( $(this).scrollTop() > 30){
@@ -22,9 +9,6 @@ $(document).ready(function(){
             $fixed.css("display", "none");
         }
     });
-   /* if (detectBrowserSize() > 1024 ){
-
-    }*/
 
 
 });
